d8196637d72eaf44d98ca58552f717556cfcef11
Markus-Otto <tommanlay4@gmail.com>
2025-11-07 16:44:14 +0100

    V 0.0.1 SPRINGBOOT init

---
+++ springbootJPA/TrainU/TrainU/src/main/java/SAE501/JLTT/TrainU/TrainUApplication.java
@@ -0,0 +1,13 @@
+package SAE501.JLTT.TrainU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainUApplication.class, args);
+	}
+
+}
---
+++ springbootJPA/TrainU/TrainU/src/test/java/SAE501/JLTT/TrainU/TrainUApplicationTests.java
@@ -0,0 +1,13 @@
+package SAE501.JLTT.TrainU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
