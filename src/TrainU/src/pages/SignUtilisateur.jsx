0917789346ec25c9134b3a1785c5f596d2ab8c5f
jorisbrusa <joris.brusa@gmail.com>
2025-12-19 22:49:03 +0100

    V 0.4.3 Correctif inscription

--- src/TrainU/src/pages/SignUtilisateur.jsx
+++ src/TrainU/src/pages/SignUtilisateur.jsx
@@ -40,7 +40,7 @@ export default function SignUtilisateur() {
 
     try {
       // Appel API pour créer l'apprenant selon le diagramme
-      const response = await fetch("http://localhost:8080/api/apprenant", {
+      const response = await fetch("http://localhost:8080/api/apprenants", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
